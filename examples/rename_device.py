90d36689e19cd9c86f70675bdfc02a0e8ad73dd9
sbcshop3 <129948335+sbcshop3@users.noreply.github.com>
2023-08-07 11:33:12 +0530

    Update rename_device.py

--- examples/rename_device.py
+++ examples/rename_device.py
@@ -1,9 +1,13 @@
+''' 
+This script rename you device from default circuitpython to your favorite name,
+Just save this script as code.py in HackyPi, then remove and re-insert.
+'''
 import storage
 
 storage.remount("/", readonly=False)
 
 m = storage.getmount("/")
-m.label = "HackyPi"    # put name which you want for your device
+m.label = "HackyPi"    # replace name which you are interested for your device
 
 storage.remount("/", readonly=True)
 
